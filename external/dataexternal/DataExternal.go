93ff25b09f64631e8dcec7d04f9f5d5f81d4104b
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-06-15 11:19:03 +0000

    chore(release): v7.0.0

--- external/dataexternal/DataExternal.go
+++ external/dataexternal/DataExternal.go
@@ -2,10 +2,10 @@ package dataexternal
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-external-go/external/v6/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-external-go/external/v7/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-external-go/external/v6/dataexternal/internal"
+	"github.com/cdktf/cdktf-provider-external-go/external/v7/dataexternal/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
